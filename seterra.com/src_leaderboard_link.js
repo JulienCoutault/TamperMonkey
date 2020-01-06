5fbfa79dc55add30328a7764b5737d6fb173e1f9
Programmateur01 <git@juliencoutault.fr>
2020-01-06 23:21:49 +0100

    Update src_leaderboard_link.js

--- seterra.com/src_leaderboard_link.js
+++ seterra.com/src_leaderboard_link.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Seterra to Speedrun.com
-// @version      1.0
+// @version      1.1
 // @namespace    https://github.com/JulienCoutault/TamperMonkey
 // @description  I'm too lazy to search the leaderbord on speedrun.com
 // @author       Programmateur01
