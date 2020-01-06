4922cbbecd93885994a2442face1d9dc836225f0
Programmateur01 <git@juliencoutault.fr>
2020-01-06 23:01:27 +0100

    Create src_leaderboard_link.js

---
+++ seterra.com/src_leaderboard_link.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         Seterra to Speedrun.com
+// @version      1.0
+// @namespace    https://github.com/JulienCoutault/TamperMonkey
+// @description  I'm too lazy to search the leaderbord on speedrun.com
+// @author       Programmateur01
+// @match        https://online.seterra.com/en/vgp/*
+// ==/UserScript==
+
+(function() {
+    function __searchInLevels(mapName) {
+        return $.ajax({
+            url: 'https://www.speedrun.com/api/v1/games/k6q4rqzd/levels',
+            method: 'GET',
+            async: true,
+            success: function(response, status, xhr) {
+                response.data.forEach(function (level) {
+                    if (level.name === mapName) {
+                        srcUrl = level.weblink;
+                    }
+                });
+            },
+        });
+    }
+    
+    function __searchInCategories(mapName) {
+        return $.ajax({
+            url: 'https://www.speedrun.com/api/v1/games/k6q4rqzd/categories',
+            method: 'GET',
+            async: true,
+            success: function(response, status, xhr) {
+                response.data.forEach(function (category) {
+                    if (category.name === mapName) {
+                        srcUrl = category.weblink;
+                    }
+                });
+            },
+        });
+    }
+    
+    let mapName = $('#lblTitleLeft').html();
+    
+    $.when(__searchInLevels(mapName), __searchInCategories(mapName)).done(function() {
+        let leaderboardLink = '<div style="font-size: 12px; margin-top: 15px; text-align: center; background-color: #0F7A4D; border-radius: 20px; padding: 15px; padding-top: 15px; padding-bottom: 20px; border: solid 1px #cccccc; width: 160px;">' +
+                '<a href=' + srcUrl + '" target="_blank" style="color: white"><center><img src="https://www.speedrun.com/images/trophy.png" style="width:60px;"></center><br>' + mapName + ' Leaderboard</a>' +
+            '</div>';
+        $('#rtcol').append(leaderboardLink);
+    });
+})();
+
+
+
+
+
+
