d3789af9f7c332c331b9a05b65d03378b21edef8
Programmateur01 <git@juliencoutault.fr>
2020-01-16 17:26:29 +0100

    Update restart.js

--- seterra.com/restart.js
+++ seterra.com/restart.js
@@ -1,10 +1,13 @@
 // ==UserScript==
 // @name         Seterra Restart game
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Restart the game use F5
 // @author       Programmateur01
 // @match        https://online.seterra.com/*/*/*
+// @match        https://online.seterra.com/*/*/*?*
+// @match        http://onlinebeta.seterra.com/*/*/*
+// @match        http://onlinebeta.seterra.com/*/*/*?*
 // @updateURL    https://raw.githubusercontent.com/JulienCoutault/TamperMonkey/master/seterra.com/restart.js
 // @downloadURL  https://raw.githubusercontent.com/JulienCoutault/TamperMonkey/master/seterra.com/restart.js
 // ==/UserScript==
