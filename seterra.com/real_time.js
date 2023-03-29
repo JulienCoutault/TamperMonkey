ee747ba9e20d7822b174ee6aba6dda798db3dbb1
Programmateur01 <git@juliencoutault.fr>
2023-03-29 15:04:43 +0200

    Update real_time.js

--- seterra.com/real_time.js
+++ seterra.com/real_time.js
@@ -4,10 +4,8 @@
 // @namespace    https://github.com/JulienCoutault/TamperMonkey
 // @description  We want the real time !
 // @author       Programmateur01
-// @match        https://online.seterra.com/*/*/*
-// @match        https://online.seterra.com/*/*/*?*
-// @match        http://onlinebeta.seterra.com/*/*/*
-// @match        http://onlinebeta.seterra.com/*/*/*?*
+// @match        https://www.geoguessr.com/seterra/*/*/*
+// @match        https://www.geoguessr.com/seterra/*/*/*?*
 // @updateURL    https://raw.githubusercontent.com/JulienCoutault/TamperMonkey/master/seterra.com/real_time.js
 // @downloadURL  https://raw.githubusercontent.com/JulienCoutault/TamperMonkey/master/seterra.com/real_time.js
 // ==/UserScript==
